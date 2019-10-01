7bf13192d50f63e76f85553cc2ad80d88f42c8e9
Nurzaly <nrzaly@gmail.com>
2019-10-01 15:37:21 +0800

    Add  expense list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,22 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
   enteredReason = '';
   inputAmount = '';
+  expensesList = [];
 
   constructor() {}
 
-  removeItem(){
-
+  removeItem(index) {
+    this.expensesList.splice(index,1);
   }
 
-  resetForm(){
-      this.enteredReason = '';
-      this.inputAmount = '';
+  resetForm() {
+    this.enteredReason = '';
+    this.inputAmount = '';
   }
 
+  addExpense() {
+    this.expensesList.push(this.enteredReason + ' RM' + this.inputAmount);
+    this.resetForm();
+  }
 }
